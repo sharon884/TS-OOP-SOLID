dd9491daef133da77fcea4c694566fd04a1695e6
sharon884 <sharonsharon9048461583@gmail.com>
2026-02-03 13:20:30 +0530

    understand why the encapsulation is still exist ? what is the use case ? how its making data more safe

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 class Product {
 
-   name : string;
-   price : number;
-   quantity : number;
+  public name : string;
+  private price : number;
+   private quantity : number;
 
 
    constructor( name : string, price: number, quantity : number ) {
@@ -11,7 +11,37 @@ class Product {
     this.quantity = quantity
 
     
-   }
+   };
+
+
+   getPrice () {
+    return this.price;
+   };
+
+   getq () {
+    return this.quantity;
+   };
+
+
+   mPrice ( amount : number ) : void {
+
+       if ( amount < 0 ) {
+        throw new Error(" amount is less than 0 is not possible");
+       };
+
+       this.price = this.price + amount;
+   };
+
+
+   mq ( q : number ) : void {
+    
+       if ( q < 0 ) {
+        throw new Error('quantiy is less than 0 its not possible');
+       };
+
+       this.quantity = this.quantity + q;
+   };
+
 };
 
 
@@ -19,4 +49,8 @@ const p = new Product('sharon',3,-5);
 
 
 p.name = 'aleena'
+p.mPrice(200);
+// p.mPrice(-1);
+p.mq(1);
+
 console.log(p)
