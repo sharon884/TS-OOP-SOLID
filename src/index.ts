8bc547d37abcc97d62a12e7bef4a4389c636c2ed
sharon884 <sharonsharon9048461583@gmail.com>
2026-02-03 13:21:17 +0530

    commented whole code for do practise on the inheritance

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,56 @@
-class Product {
+// class Product {
 
-  public name : string;
-  private price : number;
-   private quantity : number;
+//   public name : string;
+//   private price : number;
+//    private quantity : number;
 
 
-   constructor( name : string, price: number, quantity : number ) {
-    this.name = name ,
-    this.price = price, 
-    this.quantity = quantity
+//    constructor( name : string, price: number, quantity : number ) {
+//     this.name = name ,
+//     this.price = price, 
+//     this.quantity = quantity
 
     
-   };
+//    };
 
 
-   getPrice () {
-    return this.price;
-   };
+//    getPrice () {
+//     return this.price;
+//    };
 
-   getq () {
-    return this.quantity;
-   };
-
-
-   mPrice ( amount : number ) : void {
-
-       if ( amount < 0 ) {
-        throw new Error(" amount is less than 0 is not possible");
-       };
-
-       this.price = this.price + amount;
-   };
+//    getq () {
+//     return this.quantity;
+//    };
 
 
-   mq ( q : number ) : void {
+//    mPrice ( amount : number ) : void {
+
+//        if ( amount < 0 ) {
+//         throw new Error(" amount is less than 0 is not possible");
+//        };
+
+//        this.price = this.price + amount;
+//    };
+
+
+//    mq ( q : number ) : void {
     
-       if ( q < 0 ) {
-        throw new Error('quantiy is less than 0 its not possible');
-       };
+//        if ( q < 0 ) {
+//         throw new Error('quantiy is less than 0 its not possible');
+//        };
 
-       this.quantity = this.quantity + q;
-   };
+//        this.quantity = this.quantity + q;
+//    };
 
-};
-
-
-const p = new Product('sharon',3,-5);
+// };
 
 
-p.name = 'aleena'
-p.mPrice(200);
-// p.mPrice(-1);
-p.mq(1);
+// const p = new Product('sharon',3,-5);
 
-console.log(p)
+
+// p.name = 'aleena'
+// p.mPrice(200);
+// // p.mPrice(-1);
+// p.mq(1);
+
+// console.log(p)
