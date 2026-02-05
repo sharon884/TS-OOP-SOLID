29180cfadad4dc2f660c0d81fa51a5e93f3221f1
sharon884 <sharonsharon9048461583@gmail.com>
2026-02-06 04:33:24 +0530

    implemented encapsulation

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,54 @@
-interface User {
-  name : string,
+// let studentName = "Rahul";
+// let totalFee = 5000;
+// let paidAmount = 0;
+
+
+// function payFee(amount: number) {
+//   paidAmount = amount;
+// };
+
+// function printReceipt() {
+//   console.log(studentName);
+//   console.log(paidAmount);
+// };
+
+
+
+class Std {
+    
+   public studentName : string;
+    private totalFee : number;
+   private paidAmount : number
+
+
+   constructor ( studentName : string, totalFee : number  ) {
+       this.studentName = studentName;
+       this.totalFee = totalFee;
+    
+   };
+
+
+   payAmount ( amount : number ) : void {
+     if ( amount >  0)  {
+          this.paidAmount = this.paidAmount + amount;
+     }else{
+        console.log("amount is less than 0");
+     }
+   };
+
+
+   printReciept () : void {
+      console.log(this.studentName);
+      console.log(this.totalFee);
+      console.log(this.paidAmount);
+   }
 };
 
 
-const a = { name : "sharon",age:2};
-const b = { name : "aleena", age : 5};
 
+let std1 = new Std("sharon",5000);
 
-const user1 : User = a;
-const user2 : User = b;
+console.log(std1);
+std1.payAmount(5000);
+std1.printReciept;
+std1.totalFee;
