240318fc04ea0a6e086e027a7d6a2dbb1bdf957f
sharon884 <sharonsharon9048461583@gmail.com>
2026-02-05 10:22:37 +0530

    understand what is functional structural typing

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,11 @@
-// let value : string;
-// value = "sharon";
-// console.log(value)
-
-
-// let num : number = 25;
-
-// =
-
-
-interface User {
-  name : string;
-  age : number;
-  email : string;
+interface f {
+   name : string;
 };
 
-
-
-const person = {
-  name : "sharon",
-  age :29,
-  email : "sharonsharon@gmail.com",
+function user ( param : f ) : void {
+    console.log(param.name);
 };
 
+user ( { name : 'champ'} );
 
-const user : User = person;
-
-console.log(user);
-
-interface App {
-  name : number;
-  age : number;
-};
-
-
-const app  = {
-  name : 39,
-  age  : 80,
-  sum : 40,
-  avg : 35,
-};
-
-
-const n : App = app;
-
-console.log(n)
-
+user ({ name : "sharon", age : 20 });
