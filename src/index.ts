f43c56baed4a3313630ce1ed9ef03a850eb6a6c8
sharon884 <sharonsharon9048461583@gmail.com>
2026-02-05 10:13:11 +0530

    understand what is structural typing

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,47 @@
-let value : string;
-value = "sharon";
-console.log(value)
+// let value : string;
+// value = "sharon";
+// console.log(value)
 
 
-let num : number = 25;
+// let num : number = 25;
+
+// =
+
+
+interface User {
+  name : string;
+  age : number;
+  email : string;
+};
+
+
+
+const person = {
+  name : "sharon",
+  age :29,
+  email : "sharonsharon@gmail.com",
+};
+
+
+const user : User = person;
+
+console.log(user);
+
+interface App {
+  name : number;
+  age : number;
+};
+
+
+const app  = {
+  name : 39,
+  age  : 80,
+  sum : 40,
+  avg : 35,
+};
+
+
+const n : App = app;
+
+console.log(n)
 
