e0e71d95ca27d7a11b957c66f6619517e6bb464d
sharon884 <sharonsharon9048461583@gmail.com>
2026-03-01 17:35:07 +0530

    commented the whole code

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,63 @@
 
 
 
-// class BankAccount {
-//      private balance : number = 0;
+// // class BankAccount {
+// //      private balance : number = 0;
 
-//      deposit ( amount : number ) {
-//           if ( amount > 0 ) {
-//              this.balance = this.balance + amount;
-//           };
-//      };
-
-
-//      getBalance () {
-//         return this.balance;
-//      }
-// }
+// //      deposit ( amount : number ) {
+// //           if ( amount > 0 ) {
+// //              this.balance = this.balance + amount;
+// //           };
+// //      };
 
 
+// //      getBalance () {
+// //         return this.balance;
+// //      }
+// // }
 
 
 
-// interface Payment {
-//      pay( amount : number ) : void ;
+
+
+// // interface Payment {
+// //      pay( amount : number ) : void ;
      
-//     };
+// //     };
 
 
 
-//     class StripePayment implements Payment {
+// //     class StripePayment implements Payment {
          
-//         pay(amount: number  ) {
-//               console.log(" processing the payment" + amount);
-//           };
-//     };
+// //         pay(amount: number  ) {
+// //               console.log(" processing the payment" + amount);
+// //           };
+// //     };
 
 
-//     const payment : Payment = new StripePayment();
+// //     const payment : Payment = new StripePayment();
 
-//     payment.pay(100);
-
-
-
-class Animal {
-    move () {
-         console.log("this is animal")
-    }
-};
-
-
-class Dog extends Animal {
-     bark () {
-         console.log("dog is barking");
-     }
-};
+// //     payment.pay(100);
 
 
 
-const check = new Dog();
-check.bark();
-check.move();
+// class Animal {
+//     move () {
+//          console.log("this is animal")
+//     }
+// };
+
+
+// class Dog extends Animal {
+//      bark () {
+//          console.log("dog is barking");
+//      }
+// };
+
+
+
+// const check = new Dog();
+// check.bark();
+// check.move();
 
 
