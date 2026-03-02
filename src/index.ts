0adfba9f5fc69eaacbf6012100bf2d289e311760
sharon884 <sharonsharon9048461583@gmail.com>
2026-03-02 11:34:29 +0530

    basic example for the abstaction

--- src/index.ts
+++ src/index.ts
@@ -31,3 +31,40 @@
 //  sharon.deposit(1000);
 //  sharon.withdraw(400);
 //  console.log(sharon.getBalance());
+
+
+
+interface CalculateArea {
+
+    area(length: number, Width: number): number;
+};
+
+
+
+class Circle implements CalculateArea {
+    area(length: number, Width: number): number {
+
+        let result;
+        result = length * Width;
+
+        return result
+    }
+};
+
+
+class Rectangle implements CalculateArea {
+     area(length: number, Width: number): number {
+         let sum ;
+         sum = length * Width;
+         return sum;
+     }
+};
+
+
+
+
+const circle = new Circle();
+const rectangle = new Rectangle();
+
+console.log(circle.area(10,45));
+console.log(rectangle.area(20,20));
