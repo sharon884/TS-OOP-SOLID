6432b14dabc21c49e6a887773e77863a28ac2e7c
sharon884 <sharonsharon9048461583@gmail.com>
2026-03-03 10:42:03 +0530

    unknown is a safer version of any . its doing the the type cheking . we can't directly use methods on a object type is unknown . we need to prove that type to typescript by this is called type narrowing . we will use type guards for doing the type narrowing . The result of the type gurads is called type narrowing. instace of , type of are example for the type gurads . M

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,9 @@
+let value : unknown = "hellow";
+// value.toUpperCase()
 
 
-class Employee {
-    calculateBonus(): number {
-
-        return 0;
-    }
-
+if ( typeof value === "string") {
+    value.toUpperCase();
 }
 
 
-
-
-class Developer extends Employee {
-
-    private salary: number = 20200;
-
- calculateBonus(): number {
-        return this.salary * 0.1;
-    }
-
-    public printBonus( emp : Employee ) {
-         return this.calculateBonus();
-    }
-
-};
-
-
-class Manager extends Employee {
-    private salary: number = 300000;
-
-    calculateBonus(): number {
-        return this.salary * 0.2;
-    }
-};
-
-
-const dev = new Developer();
-console.log( dev.printBonus(dev));
-
-const mang = new Manager();
-console.log(mang.calculateBonus());
