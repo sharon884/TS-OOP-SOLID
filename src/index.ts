4eba82e1d77fd87ee09c087f8fe02d9f8427dbcb
sharon884 <sharonsharon9048461583@gmail.com>
2026-03-04 12:22:03 +0530

    simulate real async behavior using setTimeout.

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,52 @@
-const myPromise = new Promise<string>((resolve, reject) => {
-     let success = true;
+// const myPromise = new Promise<string>((resolve, reject) => {
+//      let success = true;
 
-     if (success) {
-          resolve("data received");
-     };
+//      if (success) {
+//           resolve("data received");
+//      };
 
 
-     if (!success) {
-          reject(" data not recevied ");
-     }
+//      if (!success) {
+//           reject(" data not recevied ");
+//      }
+// });
+
+
+
+
+// myPromise.then((data) => {
+//      console.log(data);
+// }).catch((error) => {
+//      console.log(error);
+// }).finally(() => {
+//      console.log("operation finished");
+// });
+
+
+const practise = new Promise <number> (( resolve, reject ) => {
+     let success : boolean;
+
+       setTimeout(()=> {
+           console.log("hai");
+            success = true;
+
+             if ( success ) {
+          resolve(4);
+       }else {
+           reject(5)
+       }
+       },1000);
+
+      
 });
 
 
-
-
-myPromise.then((data) => {
-     console.log(data);
+practise.then((data) => {
+      console.log("resolved",data);
 }).catch((error) => {
-     console.log(error);
-}).finally(() => {
-     console.log("operation finished");
+      console.log("error",error);
+}).finally(()=> {
+      console.log("finshed");
 });
 
 
