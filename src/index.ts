d55d3b86d7d66dcbdc8fb3c687914c602e2663b8
sharon884 <sharonsharon9048461583@gmail.com>
2026-03-04 20:35:45 +0530

    example for the conditonal types

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,8 @@
-function get<T> ( value : T ) : T {
-     return value;
-};
+type c <T> = T extends string ? "yes" : "No";
 
 
-console.log(get(3));
-console.log(get("sharon"));
-console.log(get(false));
-console.log(get([3,45,6]));
+type a = c<string>
+type b = c<number>
 
 
-// function getNumber ( value : number ) : number {
-//      return value;
-// };
 
-// console.log(getNumber(8));
-// console.log(getNumber("dsdf"))
