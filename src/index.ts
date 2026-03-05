ad7065ec7c8fe8d0c290ff215d0683446f275639
sharon884 <sharonsharon9048461583@gmail.com>
2026-03-05 10:46:27 +0530

    class Payment {      amount : number;
         constructor (amount : number) {
              this.amount = amount;
         };
    
         pay () : string {
             return
         };
    
    };
    
    class UPIPayment extends Payment {
        constructor ( amount : number) {
    
         super( amount);
        };
    
        pay() : string  {
             return ;
        };
    };
    
    class CardPayment extends Payment {
         constructor ( amount : number ) {
             super ( amount);
         };
    
         pay()  {
              return ;
         };
    };
    
    const u = new UPIPayment(8888);
    console.log(u.pay());
    
    const s = new CardPayment(4000);
    console.log(s.pay());

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,44 @@
-class Product {
-     title : string;
-     price : number ;
+class Payment {
+     amount : number;
      
-     constructor ( title : string, price : number ) {
-          this.title = title;
-          this.price = price ;
+     constructor (amount : number) {
+          this.amount = amount;
      };
      
-     getProduct () : string {
-          return ` Product : ${this.title} - Price ${this.price}`
+     
+     pay () : string {
+         return ` payment of ${this.amount} completed successfully `
      };
+     
 };
 
 
-const product = new Product("Laptop", 40000);
-console.log(product);
-console.log(product.getProduct())
+
+class UPIPayment extends Payment {
+    constructor ( amount : number) {
+        
+     super( amount);
+    };
+    
+    pay() : string  {
+         return ` UPI payment ${this.amount } completed successfully`;
+    };
+};
+
+
+class CardPayment extends Payment {
+     constructor ( amount : number ) {
+         super ( amount);
+     };
+     
+     pay()  {
+          return `card payment ${this.amount } completed successfuly`;
+     };
+};
+
+const u = new UPIPayment(8888);
+console.log(u.pay());
+
+
+const s = new CardPayment(4000);
+console.log(s.pay());
